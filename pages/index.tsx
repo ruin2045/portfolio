54dc75d8bd651f1a3310b5b04e7dcb9ddcfd087a
Ruin <mikaduki2020@gmail.com>
2023-04-05 22:38:01 +0900

    eslint set up

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
+import styles from '@/styles/Home.module.css'
+import { Inter } from 'next/font/google'
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import { useRouter } from 'next/router'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const router = useRouter()
   return (
     <>
       <Head>
@@ -14,6 +16,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <button onClick={() => router.push('test')}>GoToTest</button>
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
