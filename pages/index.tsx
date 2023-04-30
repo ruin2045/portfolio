fa74cc85b0900c512dd956b246cd2f43c54a6421
Ruin <mikaduki2020@gmail.com>
2023-04-30 22:41:03 +0900

    静的チェック修正

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import styles from '@/styles/Home.module.css'
 import { Inter } from 'next/font/google'
 import Head from 'next/head'
-import Image from 'next/image'
 import { useRouter } from 'next/router'
 
 const inter = Inter({ subsets: ['latin'] })
